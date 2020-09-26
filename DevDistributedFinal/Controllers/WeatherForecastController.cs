11f26bef95e3d93cab1ba1095535f422754553b5
Amal50 <smartprogs.com@gmail.com>
2020-09-26 00:20:08 -0400

    test

--- DevDistributedFinal/Controllers/WeatherForecastController.cs
+++ DevDistributedFinal/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace DevDistributedFinal.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" , "tesssst"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
